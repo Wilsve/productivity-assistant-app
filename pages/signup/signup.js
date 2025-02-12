9354c21cf06d472beeb97a634408b8ecc07a5051
sesar <54498315+sesar2@users.noreply.github.com>
2025-02-12 23:59:00 +0100

    register pop up

--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -1,15 +1,25 @@
 const errorMessages = ['Password must contain atleast 5 letters', 'Password must contain atleast one uppercase letter', 'Password must contain atleast one number', 'Please fill in all fields']
 
 const registerUser = () => {
-    let username = document.querySelector('.username').value
-    let password = document.querySelector('.password').value
-    let errorText = document.querySelector('.error-message')
+    const username = document.querySelector('.username').value
+    const password = document.querySelector('.password').value
+    const errorText = document.querySelector('.error-message')
+    const successMessage = document.querySelector('.success-popup')
+    const loginWindow = document.querySelector('.login-window')
+    const overlay = document.querySelector('.overlay')
 
     if(username === "" || password === ""){
         errorText.innerHTML = errorMessages[3] 
         return;
     }
+
+    const disabledElements = loginWindow.querySelectorAll('input, button');
+    disabledElements.forEach(element => {
+        element.disabled = true;
+    });
     
+    successMessage.style.display = 'flex'
+    overlay.style.display = 'block'
 
     localStorage.setItem(username, password)
     
