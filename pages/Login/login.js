18e37720f787458ac8186293bb0e7da283d85712
sesar <54498315+sesar2@users.noreply.github.com>
2025-02-13 00:54:15 +0100

    bug fixes

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -1,26 +1,37 @@
-const errorMessages = ['Password must contain atleast 5 letters', 'Password must contain atleast one uppercase letter', 'Password must contain atleast one number', 'Please fill in all fields']
+const errorMessages = [
+    'Password must contain at least 5 letters',
+    'Invalid username or password',
+    'Password must contain at least one number',
+    'Please fill in all fields'
+];
 
 const login = () => {
-    let username = document.querySelector('.username').value
-    let password = document.querySelector('.password').value
-    let errorText = document.querySelector('.error-message')
-    let userPassword = localStorage.getItem(username)
+    let username = document.querySelector('.username').value.trim();
+    let password = document.querySelector('.password').value.trim();
+    let errorText = document.querySelector('.error-message');
 
-    if(username === "" || password === "" ){
-        errorText.innerHTML = errorMessages[3] 
+    if (username === "" || password === "") {
+        errorText.innerHTML = errorMessages[3];
         return;
     }
-    // checks if passwords match
-    if(userPassword === password){
-        localStorage.setItem('loggedInUser', username)
-        window.location.href = '../Homepage/homepage.html'
-    }else{
-        errorText.innerHTML = 'Invalid username or password'
-        return
+
+    let userData = localStorage.getItem(`user_${username}`);
+    if (!userData) {
+        errorText.innerHTML = errorMessages[1];
+        return;
     }
-    
-}
+    // parse data
+    userData = JSON.parse(userData);
+    let storedPassword = userData.password;
 
-const loginButton = document.querySelector('.login-btn')
+    // check if passwords match
+    if (storedPassword === password) {
+        localStorage.setItem('loggedInUser', username);
+        window.location.href = '../Homepage/homepage.html';
+    } else {
+        errorText.innerHTML = "Invalid username or password";
+    }
+};
+
+const loginButton = document.querySelector('.login-btn');
 loginButton.addEventListener('click', login);
-
